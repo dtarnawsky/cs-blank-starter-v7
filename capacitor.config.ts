b9ca4588f509a5ed214e7f558cc4afd713bdd5b8
Damian Tarnawsky <damian@ionic.io>
2023-04-04 06:56:58 -0700

    Initial Commit

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'cs.blank.starter',
+  appName: 'cs-blank-starter',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
